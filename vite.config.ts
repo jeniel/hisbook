6cbce424f600fdb99b0635ce23b22355a7eeb3d6
unknown <joemark0008@gmail.com>
2025-07-10 16:40:43 +0800

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: true || ['*'],
+    //allowedHosts: true || ['*',""],
     port: 3000,
     watch: {
       usePolling: true,
