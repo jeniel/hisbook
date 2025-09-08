d128379780c95a114f026c2c3c42c12441c61d79
ashdroidcmd <z.wolff25@gmail.com>
2025-09-08 19:44:42 +0800

    fix

--- src/graphql/codegen/gql.ts
+++ src/graphql/codegen/gql.ts
@@ -1,24 +1,10 @@
-/* eslint-disable */
-import * as types from './graphql';
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { DocumentNode } from "graphql";
 
-const documents = [];
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- *
- *
- * @example
- * ```ts
- * const query = graphql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
- * ```
- *
- * The query argument is unknown!
- * Please regenerate the types.
- */
-export function graphql(source: string): unknown;
+// remove unused import: types
 
-export function graphql(source: string) {
-  return (documents as any)[source] ?? {};
+// Explicit type for documents
+const documents: DocumentNode[] = [];
+
+export function gql(source: string): DocumentNode {
+  return documents[source] ?? ({} as DocumentNode);
 }
-
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
