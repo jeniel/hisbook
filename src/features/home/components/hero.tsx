c8bca846db47ff8c7fd4b0a7da112219b986f7a6
ashdroidcmd <z.wolff25@gmail.com>
2025-10-22 14:59:06 +0800

    style: update hero image

--- src/features/home/components/hero.tsx
+++ src/features/home/components/hero.tsx
@@ -1,22 +1,7 @@
 export default function Hero() {
   return (
-    <section className='relative rounded-2xl bg-gradient-to-r from-blue-900 to-indigo-500 px-4 py-10 text-center text-white shadow-lg transition hover:shadow-xl md:py-20'>
-      <div>
-        <div className='mx-auto max-w-4xl flex-row justify-center'>
-          {/* Heading */}
-          <h1 className='mb-4 text-3xl font-extrabold md:text-6xl'>
-            <p>
-              Welcome to <span className='text-green-500'>ACE-</span>
-              <span className='text-red-500'>Book</span>
-            </p>
-          </h1>
-
-          {/* Subtext */}
-          <p className='text-md text-blue-50 md:text-xl'>
-            Check your Attendance or Request a Service
-          </p>
-        </div>
-      </div>
+    <section className='relative flex h-36 items-center justify-center overflow-hidden rounded-2xl shadow-lg sm:h-64 md:h-[28rem]'>
+      <div className='absolute inset-0 bg-[url("./images/cover.png")] bg-cover bg-center bg-no-repeat scale-120 md:scale-100' />
     </section>
   )
 }
