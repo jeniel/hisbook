3384b0fc82cca7f69d806c35046282cb56a09536
ashdroidcmd <z.wolff25@gmail.com>
2025-10-22 13:10:17 +0800

    updates

--- src/features/attendance/components/information.tsx
+++ src/features/attendance/components/information.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { User, Building2 } from 'lucide-react'
 import useMeQuery from '@/hooks/useMeQuery'
 import { Card, CardContent } from '@/components/ui/card'
 import { HisUser } from '../hooks/useAttendance'
@@ -12,32 +13,46 @@ export function Information({ fetchUser, user }: InformationProps) {
   const { employeeID } = useMeQuery()
 
   useEffect(() => {
-    if (employeeID !== null && employeeID !== undefined) {
-      fetchUser(employeeID.toString())
-    }
+    if (employeeID) fetchUser(employeeID.toString())
   }, [employeeID, fetchUser])
 
   return (
-    <Card>
-      <CardContent className='space-y-4'>
-        {user && (
-          <div>
-            <div className='text-xl'>
-              <p>Employee Information:</p>
-            </div>
-            <div className='text-md'>
+    <Card className='border-muted mx-auto w-full rounded-2xl border shadow-md'>
+      <CardContent className='text-md'>
+        <div>
+          <h2 className='flex items-center gap-2 text-xl font-semibold'>
+            <User className='text-primary h-5 w-5' />
+            Employee Information
+          </h2>
+        </div>
+        {user ? (
+          <>
+            <div className='flex items-center gap-2'>
+              <User className='text-muted-foreground h-4 w-4' />
               <p>
                 <span className='font-medium'>Name:</span> {user.fullName}
               </p>
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <Building2 className='text-muted-foreground h-4 w-4' />
               <p>
                 <span className='font-medium'>Department:</span>{' '}
                 {user.department.departmentName}
               </p>
-              {/* <p>
-                <span className='font-medium'>HIS ID:</span> {user.id}
-              </p> */}
             </div>
-          </div>
+
+            {/* <div className='flex items-center gap-2'>
+              <IdCard className='text-muted-foreground h-4 w-4' />
+              <p>
+                <span className='font-medium'>HIS ID:</span> {user.id}
+              </p>
+            </div> */}
+          </>
+        ) : (
+          <p className='text-muted-foreground italic'>
+            No employee data found.
+          </p>
         )}
       </CardContent>
     </Card>
